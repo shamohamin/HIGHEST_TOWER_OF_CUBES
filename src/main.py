b85c4f41b6b320a254338645157145e8ed8255fb
mohammad shafiee <shafiee.mohamin@gmail.com>
2020-04-29 19:48:13 +0430

    changed array of cubes

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from src.logic.cube import Cube
 from src.logic.ui import UI
-from src.logic.tower import permutation
+from src.logic.tower import permutation, make_highest_tower
 
 if __name__ == '__main__':
     root = tk.Tk()
@@ -9,6 +9,7 @@ if __name__ == '__main__':
     ui = UI(root)
     arr = [Cube(1), Cube(2), Cube(3)]
     permutation(arr, ui)
+    make_highest_tower(arr, ui)
     # ui.draw(arr)
     ui.mainloop()
 
