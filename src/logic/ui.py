ef973f685721363575a3ba7da1662874f2b43a9d
mohammad shafiee <shafiee.mohamin@gmail.com>
2020-04-30 16:55:32 +0430

    changed bottom up manner

--- src/logic/ui.py
+++ src/logic/ui.py
@@ -7,13 +7,22 @@ class UI(tk.Frame):
     def __init__(self, master=None, *args, **kwargs):
         super().__init__(master=master, *args, **kwargs)
         self.canvas = tk.Canvas(self, width=600, height=600)
-        self.colors = ["red", "blue", "green", "purple", "yellow", "white"]
+        self.colors_dic = {
+            1: "red",
+            2: "blue",
+            3: "green",
+            4: "purple",
+            5: "yellow",
+            6: "white",
+            7: "orange"
+        }
+        self.colors = ["white", "purple", "yellow", "blue", "green", "red"]
 
     def draw(self, arr):
-        x_pos, y_pos, last_weight = 300, 450, arr[0].weight
+        x_pos, y_pos, last_weight = 280, 450, arr[0].weight
         for x in arr:
             time.sleep(1)
-            self._make_rectangle(x_pos, y_pos, 20 * x.weight)
+            self._make_rectangle(x_pos, y_pos, 20 * x.weight, x)
 
             y_pos -= 20 * x.weight
             x_pos += 10
@@ -22,7 +31,7 @@ class UI(tk.Frame):
 
     def show_perm(self, x):
         time.sleep(0.25)
-        self._make_rectangle(200, 200, 20 * x.weight)
+        self._make_rectangle(200, 200, 20 * x.weight, x)
         self._packing()
         self.canvas.delete("all")
 
@@ -35,25 +44,25 @@ class UI(tk.Frame):
         tk.Label(self, text=error_msg).pack()
         self.pack()
 
-    def _make_rectangle(self, x, y, wieght):
+    def _make_rectangle(self, x, y, wieght, cube):
         # make back
-        self.canvas.create_rectangle(x, y, x + wieght, y + wieght, fill=self._choose_color())
+        self.canvas.create_rectangle(x, y, x + wieght, y + wieght, fill=self.colors_dic[cube.back])
         # make left
         self.canvas.create_polygon([x, y, x - 20, y + 20, x - 20, y + 20 + wieght,
-                                        x, y + wieght], fill=self._choose_color(), outline="red")
+                                        x, y + wieght], fill=self.colors_dic[cube.left], outline="red")
         # make up
         self.canvas.create_polygon([x, y, x - 20, y + 20, x - 20 + wieght,
-                                        y + 20, x + wieght, y], fill=self._choose_color(), outline="red")
+                                        y + 20, x + wieght, y], fill=self.colors_dic[cube.up], outline="red")
         # make right
         self.canvas.create_polygon([x + wieght, y, x - 20 + wieght, y + 20, x - 20 + wieght, y + 20 + wieght,
-                                        x + wieght, y + wieght], outline="red", fill=self._choose_color())
+                                        x + wieght, y + wieght], outline="red", fill=self.colors_dic[cube.right])
         # make right
         self.canvas.create_polygon([x + wieght, y + wieght, x, y + wieght, x - 20,
                                         y + 20 + wieght, x - 20 + wieght,
-                                        y + wieght + 20], outline="red", fill=self._choose_color())
+                                        y + wieght + 20], outline="red", fill=self.colors_dic[cube.down])
         # make front
         self.canvas.create_rectangle(x - 20, y + 20, x + wieght - 20,
-                                        y + wieght + 20, outline="red", fill=self._choose_color())
+                                        y + wieght + 20, outline="red", fill=self.colors_dic[cube.front])
 
     def _choose_color(self):
         return self.colors[random.randint(0, len(self.colors) - 1)]
