42bf574cdf2ce7b5da18dde11ace4114d08424e8
mohammad shafiee <shafiee.mohamin@gmail.com>
2020-04-30 16:55:58 +0430

    added Cubes to array

--- src/main.py
+++ src/main.py
@@ -7,7 +7,9 @@ if __name__ == '__main__':
     root = tk.Tk()
     root.title("TOWER OF CUBES")
     ui = UI(root)
-    arr = [Cube(1), Cube(2), Cube(3), Cube(4), Cube(5)]
+    cube = Cube(7)
+    cube.left, cube.right, cube.up, cube.down, cube.front = 7, 7, 7, 7, 7
+    arr = [Cube(1), Cube(2), Cube(3), Cube(4), Cube(5), cube, Cube(6)]
     permutation(arr, ui)
     make_highest_tower(arr, ui)
     # ui.draw(arr)
